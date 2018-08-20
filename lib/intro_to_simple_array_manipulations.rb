6069673e27e9117267dc7e85431e95a2d3fd7aa6
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 08:47:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,111 @@
+
+countries =  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+
+def using_push(array, string)
+	  string = "Niger"
+	  array.push(string)
+	end
+
+# ["Ngolo Keita", "Mafila Kanteé", "Bembeya Jazz"]
+# next_artist = "Tiken Jafakoli"
+# artists.push(next_artist)
+
+
+ $neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+ "#_________________________________________"
+ 
+ def using_unshift(neighborhoods_in_northwest_brooklyny, string)
+	  next_neighbourhood = "Brooklyn Heights"
+	  $neighborhoods_in_northwest_brooklyn.unshift(next_neighbourhood)
+	end
+
+	"#_________________________________________"
+	$great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+
+	def using_pop(array)
+	 $great_hits_of_the_nineties.pop
+	end
+
+"#_________________________________________"
+chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+
+	def pop_with_args(array)
+	  array.pop(2)
+	end
+	
+	"#_________________________________________"
+	
+		chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+
+	def pop_with_args(array)
+	  array.pop(2)
+	end
+
+"#_________________________________________"
+
+	my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+
+	def using_shift(array)
+	  array.shift
+	end
+	
+	 "#_________________________________________"
+	
+	my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+
+	def using_shift(array)
+	  array.shift
+	end
+
+ "#_________________________________________"
+	ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
+	def shift_with_args(array)
+	  array.shift(2)
+	end
+
+ "#_________________________________________"
+	my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+	more_favs = ["mario kart", "flatiron school"]
+
+
+	def using_concat(array1, array2)
+	  array1.concat(array2)
+	end
+
+
+	list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+	another_esoteric_language = "Malbolge"
+
+	def using_insert(array, element)
+	  array.insert(4, element)
+	end
+ "#_________________________________________"
+
+	captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+
+	def using_uniq(array)
+	  array.uniq
+	end
+
+ "#_________________________________________"
+	private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+
+	def using_flatten(array)
+	  array.flatten
+	end
+
+ "#_________________________________________"
+	instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+
+	def using_delete(array, string)
+	  array.delete(string)
+	  string = "Steven"
+	end
+ "#_________________________________________"
+
+	famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+
+	def using_delete_at(array, integer)
+	 array.delete_at(2)
+	end
