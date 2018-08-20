177f98edaea9f3d15471fde8fa3ac5893254a32d
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 08:55:09 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,17 +1,17 @@
 
-countries =  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+ countries =  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
 
-def using_push(array, next_country)
+ def using_push(array, next_country)
 	  next_country = "Niger"
 	  array.push(next_country)
 	end
 
-# ["Ngolo Keita", "Mafila Kanteé", "Bembeya Jazz"]
-# next_artist = "Tiken Jafakoli"
-# artists.push(next_artist)
+ # ["Ngolo Keita", "Mafila Kanteé", "Bembeya Jazz"]
+ #next_artist = "Tiken Jafakoli"
+ # artists.push(next_artist)
 
 
- neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
 
 
 	def using_unshift(array, string)
@@ -20,52 +20,38 @@ def using_push(array, next_country)
 	end
 
 
-	"#_________________________________________"
-	$great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+	great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
 
-	def using_pop(great_hits_of_the_nineties)
-	 great_hits_of_the_nineties.pop
+
+	def using_pop(array)
+	  array.pop
 	end
 
-"#_________________________________________"
-chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
 
-	def pop_with_args(array)
-	  array.pop(2)
-	end
-	
-	"#_________________________________________"
-	
-		chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+	chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+
 
 	def pop_with_args(array)
 	  array.pop(2)
 	end
 
-"#_________________________________________"
 
 	my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
 
-	def using_shift(array)
-	  array.shift
-	end
-	
-	 "#_________________________________________"
-	
-	my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
 
 	def using_shift(array)
 	  array.shift
 	end
 
- "#_________________________________________"
+
 	ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
 
 	def shift_with_args(array)
 	  array.shift(2)
 	end
 
- "#_________________________________________"
+
 	my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 	more_favs = ["mario kart", "flatiron school"]
 
@@ -78,35 +64,40 @@ chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sando
 	list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
 	another_esoteric_language = "Malbolge"
 
+
 	def using_insert(array, element)
 	  array.insert(4, element)
 	end
- "#_________________________________________"
+
 
 	captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+
 
 	def using_uniq(array)
 	  array.uniq
 	end
 
- "#_________________________________________"
+
 	private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+
 
 	def using_flatten(array)
 	  array.flatten
 	end
 
- "#_________________________________________"
+
 	instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+
 
 	def using_delete(array, string)
 	  array.delete(string)
 	  string = "Steven"
 	end
- "#_________________________________________"
+
 
 	famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
 
+
 	def using_delete_at(array, integer)
-	 array.delete_at(2)
+	  array.delete_at(2)
 	end
